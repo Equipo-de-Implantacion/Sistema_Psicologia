622e2c5fffc14981042ae8517a811c01deb3dc18
Al2jan7ro <150393851+Al2jan7ro@users.noreply.github.com>
2024-05-22 22:43:21 -0400

    Add files via upload

---
+++ JS/scroll.js
@@ -0,0 +1,39 @@
+window.sr = ScrollReveal();
+
+
+
+sr.reveal('.seccion-pequena', {
+    duration: 2000,
+    origin: 'left',
+    distance: '50px'
+});
+
+
+
+sr.reveal('#seccion1', {
+    duration: 2000,
+    origin: 'right',
+    distance: '50px'
+});
+
+sr.reveal('#seccion2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '50px'
+});
+
+
+sr.reveal('#seccion4', {
+    duration: 2000,
+    origin: 'top',
+    distance: '50px'
+});
+
+sr.reveal('#seccion5', {
+    duration: 2000,
+    origin: 'left',
+    distance: '50px'
+});
+
+
+
