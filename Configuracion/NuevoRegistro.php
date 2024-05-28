c565b1b7f23de0459aaee7de306699dfe140721e
Luisana <163352714+LuisanaBMG@users.noreply.github.com>
2024-05-27 21:39:54 -0400

    Update NuevoRegistro.php

--- Configuracion/NuevoRegistro.php
+++ Configuracion/NuevoRegistro.php
@@ -3,16 +3,12 @@ session_start();
 
 include_once('Conexion_BD.php');
 
-if (!$Conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
-}
-
 if (!isset($_SESSION['Id_Usuario'])) {
     header("Location: ../PHP/Iniciar_Sesion.php?error=error_acceso");
     exit();
 }
 
-if (isset($_POST['Tipo_Usuario']) && isset($_POST['Usuario']) && isset($_POST['Contrasena']) && isset($_POST['RContrasena'])) {
+if (isset($_POST['Tipo_Usuario']) && isset($_POST['Usuario']) && isset($_POST['Contrasena']) && isset($_POST['RContrasena']) && isset($_POST['Tipo_Usuario'])) {
 
     function validar($dato)
     {
@@ -47,18 +43,47 @@ if (isset($_POST['Tipo_Usuario']) && isset($_POST['Usuario']) && isset($_POST['C
             exit();
         } else {
             $ConsultaRegistro = "INSERT INTO usuario (Id_TipoUsuario, Usuario, Contrasena, Status_Usuario, Fecha_Registro) VALUES ('$Tipo_Usuario', '$Usuario', '$Contrasena', 'Activo', NOW())";
-            $ResultadoRegistro  = $Conexion->query($ConsultaRegistro);
+            $resultadoRegistro = $Conexion->query($ConsultaRegistro);
 
-            if ($ResultadoRegistro) {
-                header("Location: ../PHP/Nuevo_Registro.php?success=registro_exitoso");
-                exit();
-            } else {
-                header("Location: ../PHP/Nuevo_Registro.php?error=error_registro");
-                exit();
+            if ($resultadoRegistro) {
+                $Id_Usuario = $Conexion->insert_id;
+                if ($Tipo_Usuario == 2) {
+                    $Tipo_Documento = validar($_POST['Tipo_Documento']);
+                    $Documento_Id = validar($_POST['Documento_Id']);
+                    $Primer_Nombre = validar($_POST['Primer_Nombre']);
+                    $Segundo_Nombre = empty($_POST['Segundo_Nombre']) ? '' : validar($_POST['Segundo_Nombre']);
+                    $Primer_Apellido = validar($_POST['Primer_Apellido']);
+                    $Segundo_Apellido = empty($_POST['Segundo_Apellido']) ? '' : validar($_POST['Segundo_Apellido']);
+                    $Fecha_Nacimiento = validar($_POST['Fecha_Nacimiento']);
+                    $Telefono = validar($_POST['Telefono']);
+                    $Correo = validar($_POST['Correo']);
+                    $Sexo = validar($_POST['Sexo']);
+                    $Profesion = validar($_POST['Profesion']);
+                    $Especialidad = validar($_POST['Especialidad']);
+
+
+                    $idEstado = validar($_POST['Estado']);
+                    $idMunicipio = validar($_POST['Municipio']);
+                    $idParroquia = validar($_POST['Parroquia']);
+                    $idCiudad = validar($_POST['Ciudad']);
+                    $Direccion_Vivienda = validar($_POST['Direccion_Vivienda']);
+
+                    $ConsultaDir = $Conexion->prepare("INSERT INTO direccion (id_estado, id_municipio, id_parroquia, id_ciudad, Direccion_Vivienda) VALUES (?, ?, ?, ?, ?)");
+                    $ConsultaDir->bind_param("iiisi", $idEstado, $idMunicipio, $idParroquia, $idCiudad, $Direccion_Vivienda);
+                    $ConsultaDir->execute();
+                    $idDireccion = $ConsultaDir->insert_id;
+
+                    $ConsultaEmpleado = $Conexion->prepare("INSERT INTO empleado (Id_Usuario, Tipo_Documento, Documento_Id, Primer_Nombre, Segundo_Nombre, Primer_Apellido, Segundo_Apellido, Fecha_Nacimiento, Telefono, Correo, Sexo, Id_Direccion, Profesion, Especialidad, Fecha_Registro, Status_Empleado) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,NOW(),'Activo')");
+                    $ConsultaEmpleado->bind_param("issssssssssiss", $Id_Usuario, $Tipo_Documento, $Documento_Id, $Primer_Nombre, $Segundo_Nombre, $Primer_Apellido, $Segundo_Apellido, $Fecha_Nacimiento, $Telefono, $Correo, $Sexo, $idDireccion, $Profesion, $Especialidad);
+                    $ConsultaEmpleado->execute();
+
+                    header("Location: ../PHP/Nuevo_Registro.php?sucess=Registro_Exitoso");
+                }
             }
+            header("Location: ../PHP/Nuevo_Registro.php?sucess=Registro_Exitoso");
         }
     }
 } else {
-    header("Location: ../PHP/Nuevo_Registro.php");
+    header("Location: ../PHP/Nuevo_Registro.php?error=Error_Registro");
     exit();
 }
