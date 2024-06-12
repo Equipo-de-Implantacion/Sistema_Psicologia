d11a6ab911faf0ea7a4baca7cadfc1cbe055fc8d
Luisana <163352714+LuisanaBMG@users.noreply.github.com>
2024-06-12 17:53:24 -0400

    Update Citas_Agendadas.php

--- PHP/Citas_Agendadas.php
+++ PHP/Citas_Agendadas.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['Id_Usuario'])) {
     <title>Emoción Vital</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../CSS/menulateral.css">
-    <link rel="icon" href="imagenes/favicon-16x16.png" type="image/x-icon">
+    <link rel="icon" href="../imagenes/favicon-16x16.png" type="image/x-icon">
 
     
     <link href="../CSS/Citas_Agendadas.css" rel="stylesheet">
