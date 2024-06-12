6ba5601ef719bde3c712b4e0b4561992ca51929d
Luisana <163352714+LuisanaBMG@users.noreply.github.com>
2024-06-12 17:55:08 -0400

    Update Menu_Psicologo.php

--- PHP/Menu_Psicologo.php
+++ PHP/Menu_Psicologo.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['Id_Usuario'])) {
     <title>Emoción Vital</title>
 
 
-    <link rel="icon" href="imagenes/favicon-16x16.png" type="image/x-icon">
+    <link rel="icon" href="../imagenes/favicon-16x16.png" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
