1a6e980148a32f831398a2b52aef2c877578b82b
Luisana <163352714+LuisanaBMG@users.noreply.github.com>
2024-06-12 17:54:47 -0400

    Update Iniciar_Sesion.php

--- PHP/Iniciar_Sesion.php
+++ PHP/Iniciar_Sesion.php
@@ -12,7 +12,7 @@
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
   <link href="../CSS/Iniciar_Sesion.css" rel="stylesheet">
-  <link rel="icon" href="imagenes/favicon-16x16.png" type="image/x-icon">
+  <link rel="icon" href="../imagenes/favicon-16x16.png" type="image/x-icon">
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
