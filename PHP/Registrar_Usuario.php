9042766ae6fa11cdb227795fb96d5650bb48288d
Luisana <163352714+LuisanaBMG@users.noreply.github.com>
2024-06-12 17:55:59 -0400

    Update Registrar_Usuario.php

--- PHP/Registrar_Usuario.php
+++ PHP/Registrar_Usuario.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar Usuario</title>
 
-    <link rel="icon" href="imagenes/favicon-16x16.png" type="image/x-icon">
+    <link rel="icon" href="../imagenes/favicon-16x16.png" type="image/x-icon">
 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css">
